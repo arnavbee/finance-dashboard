680addeeb542f8b2d599f63aca793b399b26a128
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-19 14:08:32 +0530

    main

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,25 +3,17 @@ import {z} from "zod"
 import { handle } from 'hono/vercel'
 import {zValidator} from '@hono/zod-validator'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import authors from './authors'
+import books from './books'
+
 
 
 export const runtime = 'edge';
 
 const app = new Hono().basePath('/api')
 
-app
-
-.get('/hello', clerkMiddleware(), (c) => {
-    const auth = getAuth(c);
-
-    if(!auth?.userId){
-        return c.json({error: "Unauthorized"});
-    }
-      return c.json({
-    message: 'Hello Next.js!',
-    userId: auth.userId,
-  })
-})
+app.route('/authors', authors);
+app.route('/books', books);
 
 export const GET = handle(app)
 export const POST = handle(app)
