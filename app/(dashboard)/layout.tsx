1ecacb0dd43e2a01da7d3d439ce93fc343e4a471
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-19 17:26:16 +0530

    main

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+type Props = {
+    children: React.ReactNode;
+} 
+
+const DashboardLayout = () => {
+    return (
+        <div>
+            Dashboard Page
+        </div>
+    )
+}
+
+export default DashboardLayout;
