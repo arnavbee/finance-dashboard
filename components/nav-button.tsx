2b34dafd4fd54a31dd24b90eb91766a1c994f9eb
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-20 11:09:20 +0530

    main

---
+++ components/nav-button.tsx
@@ -0,0 +1,14 @@
+type Props = {
+    href: string;
+    label: string;
+    isActive?: boolean;
+};
+
+
+export const NavButton = ({}: Props) => {
+    return (
+        <div>
+            nav Button
+        </div>
+    )
+}
